7505faecd597b729d3cb048ddae6a1edb081468b
Tom Dawson <tomdaw92@gmail.com>
2023-03-19 16:26:22 +1000

    initial commit

---
+++ java/fixedfooter.js
@@ -0,0 +1,1 @@
+document.write('<div class="fixedfooter"><p>All images &#169; Nicolette Johnson | Website by Tom Dawson</p><script type="text/javascript" src="//s7.addthis.com/js/300/addthis_widget.js#pubid=ra-57ddf084a20ad5f5"></script></div>');
---
+++ java/head.old.js
@@ -0,0 +1,17 @@
+document.write('<link href=\'https://fonts.googleapis.com/css?family=Gentium+Basic\' rel=\'stylesheet\' type=\'text/css\'>'+
+'<link rel="apple-touch-icon" sizes="57x57" href="/favicon/apple-touch-icon-57x57.png">'+
+'<link rel="apple-touch-icon" sizes="60x60" href="/favicon/apple-touch-icon-60x60.png">'+
+'<link rel="apple-touch-icon" sizes="72x72" href="/favicon/apple-touch-icon-72x72.png">'+
+'<link rel="apple-touch-icon" sizes="76x76" href="/favicon/apple-touch-icon-76x76.png">'+
+'<link rel="icon" type="image/png" href="/favicon/favicon-32x32.png" sizes="32x32">'+
+'<link rel="icon" type="image/png" href="/favicon/favicon-96x96.png" sizes="96x96">'+
+'<link rel="icon" type="image/png" href="/favicon/favicon-16x16.png" sizes="16x16">'+
+'<link rel="manifest" href="/favicon/manifest.json">'+
+'<link rel="shortcut icon" href="/favicon/favicon.ico">'+
+'<meta name="apple-mobile-web-app-title" content="Nicolette Johnson">'+
+'<meta name="application-name" content="Nicolette Johnson">'+
+'<meta name="msapplication-TileColor" content="#ffffff">'+
+'<meta name="msapplication-config" content="/favicon/browserconfig.xml">'+
+'<meta name="theme-color" content="#ffffff">'+
+'<meta name="keywords" content="brisbane wedding photographer, brisbane lifestyle photographer, brisbane interior photographer, nicolette johnson, swsco, instagram swsco, nicolette johnson swsco">'+
+'<meta http-equiv="Cache-control" content="public">');
